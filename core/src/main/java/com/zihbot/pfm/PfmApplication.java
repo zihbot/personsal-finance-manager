cf580d5b4a31637b17e95fafdee5df8e28eb93ef
Botond Zih <zihbot@gmail.com>
2021-12-16 17:15:47 +0100

    Core initial commit

---
+++ core/src/main/java/com/zihbot/pfm/PfmApplication.java
@@ -0,0 +1,13 @@
+package com.zihbot.pfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfmApplication.class, args);
+	}
+
+}
---
+++ core/src/test/java/com/zihbot/pfm/PfmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zihbot.pfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
