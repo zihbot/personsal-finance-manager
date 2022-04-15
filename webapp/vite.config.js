a9a57e33b3f9270bd5cbde2d822d26757cd36e2a
Botond Zih <zihbot@gmail.com>
2022-04-15 19:51:13 +0200

    change to vite

---
+++ webapp/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+const path = require("path");
+console.log('route', path);
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/vars.scss";`
+      },
+    }
+  }
+});
