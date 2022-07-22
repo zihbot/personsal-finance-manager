18b517934a51b2527abcd7005f5b5d674d746c87
Botond Zih <zihbot@gmail.com>
2022-07-22 10:17:48 +0200

    ci | fix CORS

--- core/src/main/java/com/zihbot/pfm/PfmConfiguration.java
+++ core/src/main/java/com/zihbot/pfm/PfmConfiguration.java
@@ -16,6 +16,7 @@ public class PfmConfiguration {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
           registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+          registry.addMapping("/**").allowedOrigins("http://pfm-webapp");
         }
       };
     }
