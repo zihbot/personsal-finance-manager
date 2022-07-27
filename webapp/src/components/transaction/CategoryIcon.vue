43dec08cefe854eba1c7995a401fb0b00daf65cd
Botond Zih <zihbot@gmail.com>
2022-07-27 21:05:19 +0200

    web | better transaction

---
+++ webapp/src/components/transaction/CategoryIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="category-icon">
+    <i class="fas fa-question"></i>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Vue } from 'vue-class-component'
+
+export default class CategoryIcon extends Vue {
+
+}
+</script>
+
+<style lang='scss' scoped>
+.category-icon {
+  width: 3rem;
+  height: 3rem;
+  font-size: 2rem;
+  color: rgb(233, 233, 233);
+  border-radius: 50%;
+  background-color: rgb(76, 76, 190);
+  margin: auto;
+  display: flex;
+  i {
+    margin: auto;
+  }
+}
+</style>
--- webapp/src/components/transaction/OldTransaction.vue
+++ webapp/src/components/transaction/OldTransaction.vue
@@ -1,20 +1,24 @@
 <template >
   <div class="transaction">
-    <span class="tr-type">{{ transaction.type }}</span>
-    <span class="tr-accounts">
-      {{ sourceName }}
-      <i class="fas fa-arrow-right"></i>
-      {{ targetName }}
+    <CategoryIcon class="tr-category" />
+    <span class="tr-name">
+      {{ transaction.name }}
     </span>
     <span class="tr-amount">{{ transaction.amount / 100 }}</span>
   </div>
 </template>
 <script lang="ts">
 import data from '@/services/data';
-import { Vue } from 'vue-class-component'
+import { Options, Vue } from 'vue-class-component'
 import { Prop } from 'vue-property-decorator';
 import { TransactionDto } from '../../models/api/transactions'
+import CategoryIcon from './CategoryIcon.vue';
 
+@Options({
+  components: {
+    CategoryIcon,
+  }
+})
 export default class OldTransaction extends Vue {
   @Prop()
   transaction!: TransactionDto;
@@ -27,17 +31,21 @@ export default class OldTransaction extends Vue {
   }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .transaction {
   width: 100%;
   box-sizing: border-box;
-  border: 1px solid black;
   border-radius: 10px;
   background-color: whitesmoke;
   padding: 1rem;
 
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  gap: .5rem;
+
+  font-size: 1.5rem;
+  color: rgb(27, 27, 27);
 
   .tr-type, .tr-amount{
     width: 100px;
