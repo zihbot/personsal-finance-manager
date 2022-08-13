db79752abd66d1f157f048e0eb640021ea90e0fa
Botond Zih <zihbot@gmail.com>
2022-08-13 18:43:18 +0200

    web | add transaction improve

--- webapp/src/views/NewTransaction.vue
+++ webapp/src/views/NewTransaction.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="layout-page-padding-around full-height">
     <h1>New transaction</h1>
-    <number-keypad-input v-model="amount" />
-    <button class="primary full-width" id="nextButton" @click="save">Save</button>
+    <template v-if="page === 'amount'">
+      <number-keypad-input v-model="amount" />
+      <button class="primary full-width" id="nextButton" @click="next('category')">OK</button>
+    </template>
+    <div class="full-width">
+      <category-icon v-for="category of categories" class="tr-category" />
+    </div>
   </div>
 </template>
 
@@ -10,17 +15,34 @@
 import { Options, Vue } from "vue-class-component";
 
 import NumberKeypadInput from '../components/number-input/NumberKeypadInput.vue';
+import CategoryIcon from '../components/transaction/CategoryIcon.vue';
 import api from "@/services/api";
-import dataService from "@/services/data";
+import { CategoryDto } from "@/models/api/categories";
+import data from "@/services/data";
+
+type Pages = 'amount' | 'category';
 
 @Options({
   components: {
     NumberKeypadInput,
+    CategoryIcon,
   }
 })
 export default class NewTransaction extends Vue {
   amount = 0;
   saving = false;
+  page: Pages = 'amount';
+  categories = [] as CategoryDto[];
+  
+  mounted(): void {
+    data.categories.subscribe(sub => {
+      this.categories = sub.data;
+    })
+  }
+
+  next(page: Pages) {
+    this.page = page;
+  }
 
   save() {
     api.saveTransaction({
@@ -29,7 +51,7 @@ export default class NewTransaction extends Vue {
       type: 'IN'
     }).subscribe(data => {
       console.log('Finished transaction', data);
-      dataService.loadAccounts(false);
+      api.getAllAccounts().subscribe();
       this.$router.push('/home');
     }, error => {
       console.error('Cannot create transaction', error);
