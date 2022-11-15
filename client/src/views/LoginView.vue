c901ffcdfdc588fc59f1a26c058500596c190e85
Botond Zih <zihbot@gmail.com>
2022-11-15 20:39:02 +0100

    init login

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="center login-container">
+        <input name="username" v-model="username" />
+        <input name="password" v-model="password" />
+        <button @click="login">Login</button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const username = ref('');
+const password = ref('');
+
+function login() {
+    console.log('LOF')
+}
+</script>
+
+<style scoped lang="scss">
+.center {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+.login-container {
+    width: 100%;
+    max-width: 400px;
+    max-height: 300px;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    justify-content: center;
+}
+</style>
