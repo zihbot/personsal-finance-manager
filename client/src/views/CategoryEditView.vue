7eeda8239f41c16ee59f60c891869e0bbf173a99
Botond Zih <zihbot@gmail.com>
2022-11-28 18:28:34 +0100

    category edit

--- client/src/views/CategoryEditView.vue
+++ client/src/views/CategoryEditView.vue
@@ -1,7 +1,17 @@
 <template>
     <div class="edit-container">
-        <transaction-category-selector v-model="categoryId" :include-new="true">
+        <transaction-category-selector
+            v-if="!categoryId"
+            v-model="categoryId"
+            :include-new="true"
+        >
         </transaction-category-selector>
+        <div class="edited" v-if="categoryId">
+            <transaction-category
+                @click="categoryId = null"
+                :category-data="category"
+            ></transaction-category>
+        </div>
         <div v-if="categoryId">
             Color
             <div class="color-select">
@@ -12,17 +22,52 @@
                     :style="{ backgroundColor: color }"
                 ></div>
             </div>
+            Icon
+            <div class="color-select">
+                <div
+                    class="color-item"
+                    v-for="(icon, i) in icons"
+                    :key="i"
+                ><i class="fa" :class="icon"></i></div>
+            </div>
         </div>
+        <app-button
+            v-if="categoryId"
+            class="primary"
+            :disabled="!icon"
+            @click="save()"
+            >Save</app-button
+        >
     </div>
 </template>
 
 <script setup lang="ts">
+import AppButton from '@/components/AppButton.vue';
+import TransactionCategory from '@/components/TransactionCategory.vue';
 import TransactionCategorySelector from '@/components/TransactionCategorySelector.vue';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
 
 const categoryId = ref(null);
+const category = computed(
+    () =>
+        store.state.categories.find((c) => c.id === categoryId.value) ?? {
+            color: '#EEEEEE',
+            icon: '',
+        }
+);
+const color = computed(() => category.value.color);
+const icon = computed(() => category.value.icon);
 
-const colors = ref(['#AAAAAA']);
+const colors = ref(['#54426B', '#89B0EF', '#B0E298', '#3D3B8E']);
+const icons = ref(['fa-lightbulb', 'fa-train-subway', 'fa-utensils', 'fa-cart-shopping']);
+if (!colors.value.includes('')) colors.value.push('');
+
+function save() {
+    // intentional
+}
 </script>
 
 <style scoped lang="scss">
@@ -31,10 +76,15 @@ const colors = ref(['#AAAAAA']);
     flex-direction: column;
     gap: 1rem;
 }
+.edited {
+    font-size: 2em;
+    margin: auto;
+}
 .color {
     &-select {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
     }
     &-item {
         width: 2em;
